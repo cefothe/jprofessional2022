bf88e0bfc6173a648f3ac9e793f5039e92f5129a
Stefan Angelov <stefan.angelov@nexo.io>
2022-06-18 03:49:08 +0300

    Add Envers in our project

--- src/main/java/io/nexo/jprofessional/javabeer/domain/JavaBeerEvent.java
+++ src/main/java/io/nexo/jprofessional/javabeer/domain/JavaBeerEvent.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,6 +19,7 @@ import static javax.persistence.GenerationType.AUTO;
 @AllArgsConstructor
 @Builder
 @Entity
+@Audited
 public class JavaBeerEvent {
 
     @Id
